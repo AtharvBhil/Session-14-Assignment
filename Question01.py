0e34a45e3b8b973d4be7203188df2c07f1c60b27
AtharvBhil <atharvbhil08@gmail.com>
2026-07-02 18:33:02 +0530

    Array Properties

---
+++ Question01.py
@@ -0,0 +1,11 @@
+import numpy as np
+# Create a 5x6 array with random numbers from 10 to 100
+arr = np.random.randint(10, 101, (5, 6))
+print("Array:")
+print(arr)
+# Shape of the array
+print("Shape:", arr.shape)
+# Total number of elements (size)
+print("Size:", arr.size)
+# Data type (dtype)
+print("Data Type:", arr.dtype)
