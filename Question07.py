2cf5c1c27a76269e066ca3d6c0bc546984ebdf41
AtharvBhil <atharvbhil08@gmail.com>
2026-07-02 19:22:08 +0530

    Arithmetic Operations

---
+++ Question07.py
@@ -0,0 +1,10 @@
+import numpy as np
+a = np.array([10,20,30,40])
+b = np.array([1,2,3,4])
+# Arithmetic operations
+print("Addition:", a+b)
+print("Subtraction:", a-b)
+print("Multiplication:", a*b)
+print("Division:", a/b)
+print("Power:", a**b)
+print("Modulo:", a%b)
